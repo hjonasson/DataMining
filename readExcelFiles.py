0b6a70516528903951c8f20dcd46b81e7ecfe495
Hjortur <hjortur@vlan236-172-21-249-188.geosci.usyd.edu.au>
2014-08-04 12:13:09 +1000

    Tests written, problem specified better

--- readExcelFiles.py
+++ readExcelFiles.py
@@ -1,5 +1,7 @@
 import xlrd
 
+#I try to keep the design open, however some functions have to be problem specific, they will come second in the file
+
 def reader(filename,ind = 0):
 
 	#Reads in a file, for example 'Deck41_classified_final.xlsx'
@@ -21,3 +23,30 @@ def createDictionary(sheet):
 		data[metrics(i)] = values(i)
 
 	return data
+
+def testing():
+	assert type(reader('Deck41_classified_final.xlsx')) == xlrd.sheet.Sheet
+	sheet = reader('Deck41_classified_final.xlsx')
+	data = createDictionary(sheet)
+	assert sheet.cell_value(0,0) in data
+	assert sheet.ncols == len(data)
+	print 'Tests passed'
+
+#Problem specific function
+#
+#To do
+#
+#Find all points classified as mud
+#Make a window around it
+#Find all points in the window
+#Define criteria for changing mud classification
+#If criteria met, change mud classification
+
+#def findMudPoints()
+#	gives a list of mud points
+#def pointsInBox()
+#	finds all points in a box around mud point i
+#def criteria()
+#	assesses whether the points in box allow mud point to be reclassified
+#def rewrite()
+#	makes a new file to me handed to the gmt
