bd0e658b2617932f49411b9bd797bdcec7859e6a
Hjortur <hjortur@vlan236-172-21-249-188.geosci.usyd.edu.au>
2014-08-06 10:51:40 +1000

    Change of plans, deleted three days of work, will be easier from now on though

--- readExcelFiles.py
+++ readExcelFiles.py
@@ -1,4 +1,45 @@
+import re
 import xlrd
+
+
+#readTxt reads in the three column data files, the headers start with >, so they are filtered out by that
+#All line numbers are kept in order so later the files can be altered in an easy manner
+def readTxt(filename):
+
+	data = {'lon':[],'lat':[],'classif':[],'ind':[]}
+	f = open(filename)
+	lines = f.readlines()
+	lineNr = 0
+	for line in lines:
+		if line[0] != '>':
+			splitLine = re.split(r'\t+',line.strip())
+			if len(splitLine) == 3:
+				data['lon'].append(float(splitLine[0]))
+				data['lat'].append(float(splitLine[1]))
+				data['classif'].append(int(splitLine[2]))
+				data['ind'].append(lineNr)
+		lineNr += 1
+	return data
+
+#Todo
+#
+#Filter classification (mud)
+#Use filtered classification and make boxes around
+#Find all points in the box
+#Assess the points in the box
+#Rewrite the file
+
+def filterClassification(data,filter):
+
+	filteredData = {'lon':[],'lat':[],'classif':[],'ind':[]}
+	for i in range(len(data['lon'])):
+		if data['classif'][i] == filter:
+			filteredData['lon'].append(data['lon'][i])
+			filteredData['lat'].append(data['lat'][i])
+			filteredData['classif'].append(data['classif'][i])
+			filteredData['ind'].append(data['ind'][i])
+	return filteredData
+
 
 #reader and createDictionary handle the xlsx files that I need to work with
 def reader(filename,ind = 0):
@@ -23,6 +64,7 @@ def createDictionary(sheet):
 
 	return data
 
+
 #listByClassification can take in the dictionary from createDictionary, variable longitude or latitude etc. classifier is here 'Classification' (mud, ooze etc.), classifValue is 9 for mud
 def listByClassification(data,variable,classifier = 'Classification',classifValue = 9.0):
 	return [data[variable][i] for i in range(len(data[variable])) if data[classifier][i] == classifValue]
@@ -30,21 +72,6 @@ def listByClassification(data,variable,classifier = 'Classification',classifValu
 
 def findMudPoints(data):
 	return {'lon':listByClassification(data,'Longitude'),'lat':listByClassification(data,'Latitude')}
-
-def assessPoint(data,dx,dy):
-
-	mudPoints = findMudPoints(data)
-	for point in range(len(mudpoint['lon'])):
-		x = mudPoints['lon'][point]
-		y = mudPoints['lat'][point]
-		points = pointsInBox((x,y),data,dx,dy)
-		
-	#pseudocode
-	#
-	#for mudpoint
-	#if criteria met
-	#change classification
-	#	file to be changed
 
 #Finds all points in a box of size 2dx*2dy around a given point, hierarcy: pointsInBox -> BoxedPoints
 def pointsInBox(centerPoint,data,dx,dy):
@@ -57,7 +84,7 @@ def pointsInBox(centerPoint,data,dx,dy):
 
 def boxedPoints(data,xmin,xmax,ymin,ymax):
 	
-	boxed = {'lon'=[],'lat'=[],'classif'=[],'ind'=[]}
+	boxed = {'lon':[],'lat':[],'classif':[],'ind':[]}
 	for i in range(len(data['Longitude'])):
 		x = data['Longitude'][i]
 		y = data['Latitude'][i]
