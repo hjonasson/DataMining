dc941c390f678d8e005049b81347df2cec2c8855
Hjortur <hjortur@vlan236-172-21-249-188.geosci.usyd.edu.au>
2014-08-13 09:44:25 +1000

    Few days of work in this, finally altering the correct points, added tests

--- readExcelFiles.py
+++ readExcelFiles.py
@@ -4,10 +4,10 @@ import re
 #readTxt reads in the three column data files, the headers start with >, so they are filtered out by that
 #All line numbers are kept in order so later the files can be altered in an easy manner
 
-dataFile = 'seabed_lithology_v4.txt'
+dataFile = 'GMTfiles/seabed_lithology_v4.txt'
 dx = 10
 dy = 10
-minNr = 20
+minNr = 10
 
 def readTxt(filename):
 
@@ -25,17 +25,30 @@ def readTxt(filename):
 				data['ind'].append(lineNr)
 		lineNr += 1
 	return data
+"""
+f = open(dataFile)
+lines = f.readlines()
+for ind in changes['ind']:
+	splitLine = re.split(r'\t+',lines[+1].strip())
+	print splitLine[2]
+"""
 
+def testing(dataFile = 'GMTfiles/seabed_lithology_v4.txt',dx = 10,dy = 10, minNr = 10,filt = 9):
+	data = readTxt(dataFile)
+	filteredData = filterClassification(data)
+	assert all(filteredData['classif'][0] == item for item in filteredData['classif']) #All filtered items have the same classification
+	changes = classifyPoints(data,dx,dy,minNr)
+	assert all(filt == data['classif'][i] for i in changes['ind'])
 
 def filterClassification(data,filt=9):
 
 	filteredData = {'lon':[],'lat':[],'classif':[],'ind':[]}
 	for i in range(len(data['lon'])):
-		if data['classif'][i] == filter:
+		if data['classif'][i] == filt:
 			filteredData['lon'].append(data['lon'][i])
 			filteredData['lat'].append(data['lat'][i])
 			filteredData['classif'].append(data['classif'][i])
-			filteredData['ind'].append(data['ind'][i])
+			filteredData['ind'].append(i)
 	return filteredData
 
 #classifyPoints returns the points that need to be changed in a file and their lineNr
@@ -43,15 +56,15 @@ def classifyPoints(data,dx,dy,minNr,filt=9):
 	
 	filteredData = filterClassification(data,filt)
 	changes = {'ind':[],'newClass':[]}
-	for i in range(len(filteredData['lon'])):
+	for i in filteredData['ind']:
 		x = data['lon'][i]
 		y = data['lat'][i]
 		centerPoint = (x,y)
 		box = pointsInBox(centerPoint,data,dx,dy)
 		classify = classifyBox(box,minNr)
-		if classify != False
+		if classify != False:
 			#add to a list of changes to be made
-			changes['ind'].append(data['ind'][i])
+			changes['ind'].append(i)
 			changes['newClass'].append(classify)
 	print 'Changes to be made are '+str(len(changes['ind']))
 	return changes
@@ -75,12 +88,13 @@ def rewrite(filename,changes):
 #As for now, the classification is such that if there are minNr of points of a certain classification in the box, then the point is changed to that
 def classifyBox(box, minNr):
 
-	classif = box['classif']
-	mostCommon = max(set(classif),key=classif.count)
-	nrOfMostCommon = classif.count(mostCommon)
-	if nrOfMostCommon >= minNr:
+	filt = 9
+	classif = [i for i in box['classif'] if i != filt]
+	try:	
+		mostCommon = max(set(classif),key=classif.count)
+		nrOfMostCommon = classif.count(mostCommon)
 		return mostCommon
-	else:
+	except:
 		return False
 
 #Finds all points in a box of size 2dx*2dy around a given point, hierarcy: pointsInBox -> BoxedPoints
@@ -110,5 +124,5 @@ def boxedPoints(data,xmin,xmax,ymin,ymax):
 	return boxed
 
 data = readTxt(dataFile)
-changes = classifyPoints(data,dx,dy,minNr)
+changes2 = classifyPoints(data,dx,dy,minNr)
 rewrite(dataFile,changes)
